9355da9e5fe73c6ceeeca157d05a6c37477fe244
IT21711978 <it21713262@my.sliit.lk>
2024-12-03 13:56:51 +0530

    password hashing

--- server/signUp.php
+++ server/signUp.php
@@ -5,25 +5,23 @@ include('./dbConnect.php');
 $userName = $_POST['userName'];
 $pass = $_POST['pass'];
 
+// Hash the password before storing it in the database
+$hashedPass = password_hash($pass, PASSWORD_DEFAULT);
+
 $sql = "SELECT * FROM `users` WHERE `userName`='" . $userName . "'";
 
 $result = mysqli_query($con, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-
     echo "Registered";
-
 } else {
+    $sql1 = "INSERT INTO `users` (`userName`, `password`, `highScore`) VALUES ('" . $userName . "', '" . $hashedPass . "', 0);";
 
-    $sql1 = "INSERT INTO `users` (`userName`,`password`,`highScore`) VALUES ('" . $userName . "','" . $pass . "',0);";
-
-    if(mysqli_query($con, $sql1)){
-
+    if (mysqli_query($con, $sql1)) {
         echo "Success";
+    } else {
+        echo "Error: " . mysqli_error($con);
     }
-    
 }
-
-
 
 ?>
